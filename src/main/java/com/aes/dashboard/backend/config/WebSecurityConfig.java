0e62ae91f17d8a82e64857a97669f3350e3e52b3
Heraldo Valenti <heraldovalenti@gmail.com>
2021-01-05 10:07:19 -0300

    Added basic security configuration

---
+++ src/main/java/com/aes/dashboard/backend/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.aes.dashboard.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/api/health-check").hasRole("HEALTH_CHECK")
+                .antMatchers("/**").hasRole("USER")
+                .and()
+            .httpBasic()
+        ;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("monitor")
+                    .password(passwordEncoder()
+                        .encode("SaltaLaLinda01"))
+                    .roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("healthChecker")
+                    .password(passwordEncoder()
+                        .encode("h_C01"))
+                    .roles("HEALTH_CHECK", "USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
