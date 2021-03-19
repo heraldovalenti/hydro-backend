24759d8c054589901380c61da20ea3b67da66e32
Heraldo Valenti <heraldovalenti@gmail.com>
2021-03-19 11:59:13 -0300

    Improved data origin update processes
    - Now Observations will NOT be updated if they previously existed.
    Instead, they will be discarded and only new observations will be
    persisted to avoid iterating each existing observation and improve
    execution time. This was done for data origins AES, INTASiga2,
    INTAAnterior and SNIH - WeatherUnderground remains the same.

--- src/main/java/com/aes/dashboard/backend/service/ObservationService.java
+++ src/main/java/com/aes/dashboard/backend/service/ObservationService.java
@@ -20,8 +20,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ObservationService {
@@ -57,10 +60,12 @@ public class ObservationService {
 
     @Transactional
     public void updateAesObservations() {
+        LOGGER.info("Starting update for AES observations...");
         DataOrigin aesDataOrigin = dataOriginService.getAesDataOrigin();
         List<DataItem> latestDataItems = aesDataService.getLatestData();
         List<StationDataOrigin> aesStationDataOriginList = stationDataOriginRepository.findByDataOrigin(aesDataOrigin);
         for (StationDataOrigin aesStationDataOrigin : aesStationDataOriginList) {
+            List<Observation> observations = new LinkedList<>();
             latestDataItems.stream()
                     .filter(item ->
                             aesStationDataOrigin.getExternalStationId().equals(item.getId())
@@ -76,13 +81,16 @@ public class ObservationService {
                         Optional<MeasurementUnit> unit = measurementUnitService.getByAlias(dataItem.getUnit());
                         if (unit.isPresent()) observation.setUnit(unit.get());
                         else observation.setUnit(aesStationDataOrigin.getDefaultUnit());
-                        this.updateOrCreateObservation(observation);
+                observations.add(observation);
             });
+            updateObservationsForStationOrigin(aesStationDataOrigin, observations);
         }
+        LOGGER.info("Update for AES observations completed");
     }
 
     @Transactional
     public void updateWeatherUndergroundObservations() {
+        LOGGER.info("Starting update for WeatherUnderground observations...");
         DataOrigin WUDataOrigin = dataOriginService.getWeatherUndergroundDataOrigin();
         List<StationDataOrigin> WUStationDataOriginList = stationDataOriginRepository.findByDataOrigin(WUDataOrigin);
         for (StationDataOrigin WUStationDataOrigin : WUStationDataOriginList) {
@@ -102,48 +110,55 @@ public class ObservationService {
             observation.setUnit(WUStationDataOrigin.getDefaultUnit());
             this.updateOrCreateObservation(observation);
         }
+        LOGGER.info("Update for WeatherUnderground observations completed");
     }
 
     @Transactional
     public void updateINTASiga2Observations() {
+        LOGGER.info("Starting update for INTASiga2 observations...");
         DataOrigin intaDataOrigin = dataOriginService.getINTASiga2DataOrigin();
         List<StationDataOrigin> intaStationDataOriginList = stationDataOriginRepository.findByDataOrigin(intaDataOrigin);
         for (StationDataOrigin intaStationDataOrigin : intaStationDataOriginList) {
+            List<Observation> observations = new LinkedList<>();
             List<INTASiga2DataItem> intaDataItems = intaSiga2DataService.getObservationData(
                     intaStationDataOrigin.getExternalStationId());
-            intaDataItems.stream()
-                    .forEach(dataItem -> {
-                        Observation observation = new Observation();
-                        observation.setDimension(intaStationDataOrigin.getDimension());
-                        observation.setStation(intaStationDataOrigin.getStation());
-                        observation.setDataOrigin(intaDataOrigin);
-                        observation.setTime(dataItem.getFecha());
-                        observation.setValue(dataItem.getPrecipitacion());
-                        observation.setUnit(intaStationDataOrigin.getDefaultUnit());
-                        this.updateOrCreateObservation(observation);
-                    });
+            for(INTASiga2DataItem dataItem : intaDataItems) {
+                Observation observation = new Observation();
+                observation.setDimension(intaStationDataOrigin.getDimension());
+                observation.setStation(intaStationDataOrigin.getStation());
+                observation.setDataOrigin(intaDataOrigin);
+                observation.setTime(dataItem.getFecha());
+                observation.setValue(dataItem.getPrecipitacion());
+                observation.setUnit(intaStationDataOrigin.getDefaultUnit());
+            }
+            updateObservationsForStationOrigin(intaStationDataOrigin, observations);
         }
+        LOGGER.info("Update for INTASiga2 observations completed");
     }
 
     @Transactional
     public void updateINTAAnteriorObservations() {
+        LOGGER.info("Starting update for INTAAnterior observations...");
         DataOrigin intaDataOrigin = dataOriginService.getINTAAnteriorDataOrigin();
         List<StationDataOrigin> intaStationDataOriginList = stationDataOriginRepository.findByDataOrigin(intaDataOrigin);
         for (StationDataOrigin intaStationDataOrigin : intaStationDataOriginList) {
+            List<Observation> observations = new LinkedList<>();
             List<INTAAnteriorDataItem> intaDataItems = intaAnteriorDataService.getObservationData(
                     intaStationDataOrigin.getExternalStationId());
-            intaDataItems.stream()
-                    .forEach(dataItem -> {
-                        Observation observation = new Observation();
-                        observation.setDimension(intaStationDataOrigin.getDimension());
-                        observation.setStation(intaStationDataOrigin.getStation());
-                        observation.setDataOrigin(intaDataOrigin);
-                        observation.setTime(dataItem.getDate());
-                        observation.setValue(dataItem.getLluvia());
-                        observation.setUnit(intaStationDataOrigin.getDefaultUnit());
-                        this.updateOrCreateObservation(observation);
-                    });
+            for (INTAAnteriorDataItem dataItem : intaDataItems) {
+                Observation observation = new Observation();
+                observation.setDimension(intaStationDataOrigin.getDimension());
+                observation.setStation(intaStationDataOrigin.getStation());
+                observation.setDataOrigin(intaDataOrigin);
+                observation.setTime(dataItem.getDate());
+                observation.setValue(dataItem.getLluvia());
+                observation.setUnit(intaStationDataOrigin.getDefaultUnit());
+                observations.add(observation);
+            }
+            updateObservationsForStationOrigin(intaStationDataOrigin, observations);
         }
+        LOGGER.info("Update for INTAAnterior observations completed");
+
     }
 
     @Transactional
@@ -152,6 +167,7 @@ public class ObservationService {
         DataOrigin snihDataOrigin = dataOriginService.getSNIHDataOrigin();
         List<StationDataOrigin> snihStationDataOriginList = stationDataOriginRepository.findByDataOrigin(snihDataOrigin);
         for (StationDataOrigin snihStationDataOrigin : snihStationDataOriginList) {
+            List<Observation> observations = new LinkedList<>();
             Optional<SNIHDataCode> aux = SNIHDataCode.codeForMeasurementDimension(snihStationDataOrigin.getDimension());
             if (aux.isEmpty()) {
                 LOGGER.warn("SNIHDataCode not found for MeasurementDimension {}", snihStationDataOrigin.getDimension());
@@ -167,10 +183,30 @@ public class ObservationService {
                 observation.setTime(dataItem.getDateTime());
                 observation.setValue(dataItem.getValue());
                 observation.setUnit(snihStationDataOrigin.getDefaultUnit());
-                this.updateOrCreateObservation(observation);
+                observations.add(observation);
             }
+            updateObservationsForStationOrigin(snihStationDataOrigin, observations);
         }
         LOGGER.info("Update for SNIH observations completed");
+    }
+
+    @Transactional
+    public void updateObservationsForStationOrigin(StationDataOrigin stationDataOrigin, List<Observation> observations) {
+        Optional<Observation> lastObservation = observationRepository.findFirstByStationAndDimensionOrderByTimeDesc(
+                stationDataOrigin.getStation(), stationDataOrigin.getDimension());
+        if (lastObservation.isPresent()) {
+            LOGGER.info("Last observation for station \"{}\" ({}) and dimension \"{}\" found ({}), discarding previous observations...",
+                    stationDataOrigin.getStation().getDescription(),
+                    stationDataOrigin.getStation().getId(),
+                    stationDataOrigin.getDimension().getDescription(),
+                    lastObservation.get().getTime());
+            observations.stream()
+                    .filter(o -> o.getTime().isAfter(lastObservation.get().getTime()))
+                    .forEach(this::updateOrCreateObservation);
+        } else {
+            observations.stream()
+                    .forEach(this::updateOrCreateObservation);
+        }
     }
 
     @Transactional
