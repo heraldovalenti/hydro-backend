6ee2252fdf707b80599ac5b5f4f17aa004ddae5e
Heraldo Valenti <heraldovalenti@gmail.com>
2021-06-10 10:32:17 -0300

    Fixed accumulation calculation when negative observations are present

--- src/main/java/com/aes/dashboard/backend/model/accumulation/AesRainAccumulator.java
+++ src/main/java/com/aes/dashboard/backend/model/accumulation/AesRainAccumulator.java
@@ -2,16 +2,21 @@ package com.aes.dashboard.backend.model.accumulation;
 
 import com.aes.dashboard.backend.model.Observation;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AesRainAccumulator extends RainAccumulator {
 
     @Override
     public double accumulate(List<Observation> observations) {
-        if (observations.size() == 1) return 0;
-        List<Observation> sorted = this.sortObservations(observations);
-        double firstObservation = Math.max(0, sorted.get(0).getValue());
-        double lastObservation = Math.max(0, sorted.get(observations.size() - 1).getValue());
+        List<Observation> purgedList = observations.stream()
+                .filter(o -> o.getValue() >= 0) // filter out observations with negative value
+                .collect(Collectors.toList());
+        if (purgedList.size() == 1) return 0;
+        LinkedList<Observation> sorted = new LinkedList<>(this.sortObservations(purgedList));
+        double firstObservation = sorted.getFirst().getValue();
+        double lastObservation = sorted.getLast().getValue();
         double diff = lastObservation - firstObservation;
         return (diff < 0) ? 0.0 : diff;
     }
