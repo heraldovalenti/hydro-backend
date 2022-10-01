eecb75834a62bd4ea755c41cfd77afaee3125b7e
Heraldo Valenti <heraldovalenti@gmail.com>
2022-10-01 00:56:17 -0300

    Changed Weatherlink dates timezone to UTC

--- src/main/java/com/aes/dashboard/backend/service/weatherlinkData/TimestampProvider.java
+++ src/main/java/com/aes/dashboard/backend/service/weatherlinkData/TimestampProvider.java
@@ -9,13 +9,13 @@ import java.time.ZoneId;
 public class TimestampProvider {
 
     public static Long getTimestamp(LocalDateTime date) {
-        ZoneId zoneId = ZoneId.of(GlobalConfigs.SALTA_ZONE_ID);
+        ZoneId zoneId = ZoneId.of(GlobalConfigs.UTC_ZONE_ID);
         long epochSeconds = date.atZone(zoneId).toEpochSecond();
         return epochSeconds;
     }
 
     public static LocalDateTime getLocalDate(Long epochSeconds) {
-        ZoneId zoneId = ZoneId.of(GlobalConfigs.SALTA_ZONE_ID);
+        ZoneId zoneId = ZoneId.of(GlobalConfigs.UTC_ZONE_ID);
         LocalDateTime ldt = Instant.ofEpochSecond(epochSeconds).atZone(zoneId).toLocalDateTime();
         return ldt;
     }
