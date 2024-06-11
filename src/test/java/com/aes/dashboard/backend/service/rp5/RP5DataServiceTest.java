0a00c43ac43906507d6da7dc04ec88679a8c4e03
Heraldo Valenti <heraldovalenti@gmail.com>
2024-06-11 08:34:42 -0700

    Improved RP5 data service test

--- src/test/java/com/aes/dashboard/backend/service/rp5/RP5DataServiceTest.java
+++ src/test/java/com/aes/dashboard/backend/service/rp5/RP5DataServiceTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 @SpringBootTest
 class RP5DataServiceTest {
 
-
     private static final String stationId = "Archivo_de_tiempo_en_Salta_(aeropuerto)";
     @Autowired
     private RP5DataService service;
@@ -19,7 +18,8 @@ class RP5DataServiceTest {
     @Test
     void getTodayObservationData() {
         List<RP5Row> result = service.getObservationData(stationId);
-        Assertions.assertEquals(7, result.size());
+        LocalDateTime now = LocalDateTime.now();
+        Assertions.assertEquals(now.getDayOfYear(), result.get(0).getDateTime().getDayOfYear());
     }
 
     @Test
@@ -27,6 +27,12 @@ class RP5DataServiceTest {
         LocalDateTime ldt = LocalDateTime.of(2024, 1, 15, 0, 0); // 15.01.2024
         List<RP5Row> result = service.getObservationData(stationId, ldt, RP5Period.MONTH, 0);
         Assertions.assertEquals(110, result.size());
+        Assertions.assertEquals(
+                LocalDateTime.of(2024, 1, 16, 0, 0),
+                result.get(0).getDateTime());
+        Assertions.assertEquals(
+                LocalDateTime.of(2023, 12, 17, 6, 0),
+                result.get(109).getDateTime());
     }
 
     @Test
